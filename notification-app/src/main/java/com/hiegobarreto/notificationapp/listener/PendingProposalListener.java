5bc54cd5e29624f6463a11ab082411ba87394653
Hiego <hiegobarreto@gmail.com>
2024-05-30 14:52:58 -0300

    notification completed proposal

--- notification-app/src/main/java/com/hiegobarreto/notificationapp/listener/PendingProposalListener.java
+++ notification-app/src/main/java/com/hiegobarreto/notificationapp/listener/PendingProposalListener.java
@@ -16,6 +16,20 @@ public class PendingProposalListener {
     @RabbitListener(queues = "${rabbitmq.queue.pending.proposal}")
     public void pendingProposal(Proposal proposal){
         String message = String.format(MessageConstant.PROPOSAL_ANALYSIS, proposal.getUsuario().getNome());
-        notificationSnsService.notify(proposal.getUsuario().getTelefone(),message);
+        notificationSnsService.notify(proposal.getUsuario().getTelefone(), message);
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queue.completed.proposal}")
+    public void completedProposal(Proposal proposal){
+
+        String message;
+
+        if(proposal.getAprovada()){
+            message = String.format(MessageConstant.PROPOSAL_APPROVED, proposal.getUsuario().getNome());
+        }else{
+            message = String.format(MessageConstant.PROPOSAL_DENIED, proposal.getUsuario().getNome(), proposal.getObservacao());
+        }
+
+        notificationSnsService.notify(proposal.getUsuario().getTelefone(), message);
     }
 }
